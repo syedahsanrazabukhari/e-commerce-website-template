b3329542a6a3b0e3ca91a7a646a6c6db53abba83
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2024-11-25 22:36:18 +0500

    code to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
@@ -68,6 +66,59 @@ export default function Home() {
               <div className="pl-8">
                 <h2>30,000+</h2>
                 <span className="opacity-60">Happy Customers</span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        {/* Sponsers Brand Names*/}
+        <section>
+          <div className=" bg-black flex gap-[106px] px-[100px] py-[44px]">
+            <img src="/versac.png" alt="Sponsers-1" />
+            <img src="/zara.png" alt="Sponsers-2" />
+            <img src="/gucci.png" alt="Sponsers-3" />
+            <img src="/prada.png" alt="Sponsers-4" />
+            <img src="/calvin.png" alt="Sponsers-5" />
+          </div>
+        </section>
+
+        {/* Heading of new Arrival */}
+        <p className="font-integral font-bold text-[48px] leading-[57.6px] text-center mt-[72px] mb-[55px]">NEW ARRIVALS</p>
+
+        {/* New Arrival Products */}
+        <section>
+          <div className="flex  mx-[100px] ">
+            <div className=" space-y-4">
+              <img className="rounded-[20px]" src="/black-shirt.svg" alt="failed" />
+              <div className="space-y-2" >
+                <p className="font-satoshi font-bold text-[20px] leading-[27px]">T-SHIRT WITH TAPE DETAILS</p>
+                <img src="/star-1.svg" alt="failed" />
+                <span className="font-satoshi font-bold text-[24px] leading-[32.4px]">$120</span>
+              </div>
+            </div>
+            <div className=" ml-[100px] space-y-4">
+              <img className="rounded-[20px]" src="/black-shirt.svg" alt="failed" />
+              <div className="space-y-2" >
+                <p className="font-satoshi font-bold text-[20px] leading-[27px]">T-SHIRT WITH TAPE DETAILS</p>
+                <img src="/star-1.svg" alt="failed" />
+                <span className="font-satoshi font-bold text-[24px] leading-[32.4px]">$120</span>
+              </div>
+            </div>
+            <div className=" ml-[100px] space-y-4">
+              <img className="rounded-[20px]" src="/black-shirt.svg" alt="failed" />
+              <div className="space-y-2" >
+                <p className="font-satoshi font-bold text-[20px] leading-[27px]">T-SHIRT WITH TAPE DETAILS</p>
+                <img src="/star-1.svg" alt="failed" />
+                <span className="font-satoshi font-bold text-[24px] leading-[32.4px]">$120</span>
+              </div>
+            </div>
+            <div className=" ml-[100px] space-y-4">
+              <img className="rounded-[20px]" src="/black-shirt.svg" alt="failed" />
+              <div className="space-y-2" >
+                <p className="font-satoshi font-bold text-[20px] leading-[27px]">T-SHIRT WITH TAPE DETAILS</p>
+                <img src="/star-1.svg" alt="failed" />
+                <span className="font-satoshi font-bold text-[24px] leading-[32.4px]">$120</span>
               </div>
             </div>
           </div>
