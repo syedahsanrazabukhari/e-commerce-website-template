12ff094ab4ca5eea047f71836878910cf118fd0e
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2024-12-02 01:45:02 +0500

    Complete code to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,7 +234,7 @@ export default function Home() {
                 <p className="font-satoshi font-bold text-[16px] sm:text-[20px] leading-[22px] align-text-top">Sarah M.</p>
                 <img src="/green-tick.svg" alt="failed" />
               </div>
-              <p className="mt-2 sm:mt-[12px] font-satoshi font-normal text-[14px]  sm:text-[16px] leading-5 sm:leading-[22px] w-[310px] sm:w-[336px]">"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
+              <p className="mt-2 sm:mt-[12px] font-satoshi font-normal text-[14px]  sm:text-[16px] leading-5 sm:leading-[22px] w-[310px] sm:w-[336px]">"I am blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I have bought has exceeded my expectations.”</p>
             </div>
             <div className="w-[408px] border border-#0000001A rounded-[20px] py-[28px] px-[32px] hidden sm:block">
               <img src="/5-star.svg" alt="failed" />
@@ -242,7 +242,7 @@ export default function Home() {
                 <p className="font-satoshi font-bold text-[20px] leading-[22px] align-text-top">Alex K.</p>
                 <img src="/green-tick.svg" alt="failed" />
               </div>
-              <p className="mt-[12px] font-satoshi font-normal text-[16px] leading-[22px] w-[344px]">"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”</p>
+              <p className="mt-[12px] font-satoshi font-normal text-[16px] leading-[22px] w-[344px]"> "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”</p>
             </div>
             <div className="w-[408px] border border-#0000001A rounded-[20px] py-[28px] px-[32px] hidden sm:block">
               <img src="/5-star.svg" alt="failed" />
@@ -250,7 +250,7 @@ export default function Home() {
                 <p className="font-satoshi font-bold text-[20px] leading-[22px] align-text-top">James L.</p>
                 <img src="/green-tick.svg" alt="failed" />
               </div>
-              <p className="mt-[12px] font-satoshi font-normal text-[16px] leading-[22px] w-[344px]">"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”</p>
+              <p className="mt-[12px] font-satoshi font-normal text-[16px] leading-[22px] w-[344px]">"As someone who is always on the lookout for unique fashion pieces, I am thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”</p>
             </div>
           </div>
         </section>
